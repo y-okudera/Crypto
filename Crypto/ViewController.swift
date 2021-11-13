04a951895d995d676a911462518618353eea4302
yuoku <appledev.yuoku@gmail.com>
2021-11-13 23:26:40 +0900

    Create iOS project.

---
+++ Crypto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Crypto
+//
+//  Created by Yuki Okudera on 2021/11/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
